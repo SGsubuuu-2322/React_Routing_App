0645ccbc39a21fa4e35aaf9064866338a2e9dcbc
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-09 09:11:52 +0530

    Segregated the codes for Routing in utils folder...

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 // import React from "react";
-import { Routes, Route } from "react-router-dom";
 import Nav from "./Components/Nav";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import Users from "./Components/Users";
+import Routing from "./Utils/Routing";
 
 function App() {
   return (
     <div>
       <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <Routing />
     </div>
   );
 }
---
+++ src/Utils/Routing.jsx
@@ -0,0 +1,17 @@
+// import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../Components/Home";
+import Users from "../Components/Users";
+import About from "../Components/About";
+
+const Routing = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/users" element={<Users />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+};
+
+export default Routing;
