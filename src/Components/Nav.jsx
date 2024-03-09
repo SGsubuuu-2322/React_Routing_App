9555938c087d570d02b43a9dad897de69536f958
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-09 10:07:41 +0530

    Added Navigation code for navbar in our app using react-router-dom with its NavLink

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,12 +1,45 @@
 // import React from 'react'
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav className="text-xl font-semibold flex justify-center gap-10 mt-10 bg-red-200">
-      <Link to="/">Home</Link>
-      <Link to="/users">Users</Link>
-      <Link to="/about">About</Link>
+      <NavLink to="/">
+        {(e) => {
+          return (
+            <span
+              className={[
+                e.isActive ? "text-red-400" : "",
+                e.isActive ? "font-bold" : "",
+              ].join(" ")}
+            >
+              Home
+            </span>
+          );
+        }}
+      </NavLink>
+      <NavLink
+        className={(e) => {
+          return [
+            e.isActive ? "text-red-400" : "",
+            e.isActive ? "font-bold" : "",
+          ].join(" ");
+        }}
+        to="/users"
+      >
+        Users
+      </NavLink>
+      <NavLink
+        style={(e) => {
+          return {
+            color: e.isActive ? "Tomato" : "",
+            fontWeight: e.isActive ? "bold" : "",
+          };
+        }}
+        to="/about"
+      >
+        About
+      </NavLink>
     </nav>
   );
 };
